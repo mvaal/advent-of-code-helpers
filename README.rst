8555914593777e2f3c95b92eb9f9eb02f7cbefd9
Marcus Vaal <mjvaal@gmail.com>
2018-12-02 23:04:20 -0600

    Added pypi badge to readme
    Fixed some incorrect data in the setup.py file

--- README.rst
+++ README.rst
@@ -2,6 +2,7 @@ Advent of Code helpers
 ======================
 .. image:: https://travis-ci.org/mvaal/advent-of-code-helpers.svg?branch=master
     :target: https://travis-ci.org/mvaal/advent-of-code-helpers
-
+.. image:: https://badge.fury.io/py/advent-of-code-helpers.svg
+    :target: https://badge.fury.io/py/advent-of-code-helpers
 
 Advent of Code helper functions
--- setup.py
+++ setup.py
@@ -6,13 +6,12 @@ with open("README.rst", "r") as fh:
 setuptools.setup(
     name="advent-of-code-helpers",
     version="0.0.2",
-    author="Example Author",
-    author_email="author@example.com",
+    author="Marcus Vaal",
     license="MIT",
     description="Advent of Code helper functions",
     long_description=long_description,
     long_description_content_type="text/x-rst",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/mvaal/advent-of-code-helpers",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
