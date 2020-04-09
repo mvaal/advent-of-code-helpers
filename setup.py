31b2645123bf0b97152520b0c68da3e9c7f943fa
Marcus Vaal <mjvaal@gmail.com>
2020-04-08 22:10:09 -0500

    Add HISTORY.rst, update from default git workflow

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 import setuptools
 
 with open("README.rst", "r") as fh:
-    long_description = fh.read()
+    readme = fh.read()
 
-install_requires = ['pytest']
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+install_requires = []
+
+test_requirements = ['pytest>=3']
 
 setuptools.setup(
     name="advent-of-code-helpers",
-    version="0.1.0",
+    version="0.1.1",
     author="Marcus Vaal",
     license="MIT",
     description="Advent of Code helper functions",
-    long_description=long_description,
+    long_description= readme + '\n\n' + history,
     long_description_content_type="text/x-rst",
     url="https://github.com/mvaal/advent-of-code-helpers",
     packages=setuptools.find_packages(),
@@ -21,5 +26,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries"
     ],
-    install_requires=install_requires
+    install_requires=install_requires,
+    test_requirements=test_requirements
 )
